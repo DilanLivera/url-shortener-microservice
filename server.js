9148d60b14108d8f216b51d49c9570f5bcfffd00
DilanLivera <DilanLivera@users.noreply.github.com>
2019-06-24 10:01:22 +1000

    setup files and add modules

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+const listener = app.listen(PORT, () => console.log(`App is listening on port ${listener.address().port}`));
