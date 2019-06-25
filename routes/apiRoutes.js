973fdd20f7e97bd822c204f387c2dedc0ebe943c
DilanLivera <DilanLivera@users.noreply.github.com>
2019-06-25 12:12:11 +1000

    add functionality to check if the url exists before adding

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,27 +15,44 @@ router.get('/:shortUrl', (req, res) => {
         res.status(404).json({"error":"invalid shortURL"});
       }
     })
-    .catch((err) => res.send(`Oops, something went wrong -> ${err}`))
+    .catch(error);
 });
 
 router.post('/new', (req, res) => {
   let originalUrl = req.body.url;
 
-  //add url
-  db.Url.find()
-    .then((urls) => { 
-      let shortUrl = urls.length + 1;
-      let data = {
-        originalUrl,
-        shortUrl
-      }      
-      return db.Url.create(data);
-    })
-    .then((newUrl) => {
-      let { originalUrl, shortUrl } = newUrl;
-      res.status(201).json({ originalUrl, shortUrl })
-    })
-    .catch((err) => res.send(`Oops, something went wrong -> ${err}`))
+  //check if url exist
+  db.Url.find({ originalUrl })
+        .then((url) => {
+          // if exists 
+          if(url.length) {
+            let { originalUrl, shortUrl } = url[0];
+            res.status(201).json({ originalUrl, shortUrl });
+          }
+          else {
+            db.Url.find()
+              .then((urls) => { 
+                let shortUrl = urls.length + 1;
+                let data = {
+                  originalUrl,
+                  shortUrl
+                }
+                //add url      
+                return db.Url.create(data);
+              })
+              .then((newUrl) => {
+                let { originalUrl, shortUrl } = newUrl;
+                res.status(201).json({ originalUrl, shortUrl })
+              })
+              .catch(error);
+          }
+        })
+        .catch(error);
 });
+
+//error
+function error(err) {
+  res.send(`Oops, something went wrong -> ${err}`);
+}
 
 module.exports = router;
